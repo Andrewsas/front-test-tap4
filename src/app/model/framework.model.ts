d1ce0615e4a15afe70ebfe8a3a327fc3285b863f
Andrewsas <s.andrew.santos@gmail.com>
2019-07-04 18:42:43 -0400

    ajustes model

--- src/app/model/framework.model.ts
+++ src/app/model/framework.model.ts
@@ -1,5 +1,14 @@
 import { BaseModel } from './base-model';
 
 export class Framework extends BaseModel {
-
+    'name': string;
+    'creator': string;
+    'site': string;
+    'language': string;
+    'id_language': string;
+    'type': string;
+    'year': number;
+    'version': string;
+    'against': string;
+    'pro': string;
 }
--- src/app/model/language.model.ts
+++ src/app/model/language.model.ts
@@ -1,5 +1,10 @@
 import { BaseModel } from './base-model';
 
 export class Language extends BaseModel {
-
+    name: string;
+    creator: string;
+    site: string;
+    type: string;
+    year: number;
+    version: string;
 }
